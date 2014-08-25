77ccdbdd5142c7a43c73a325bbd37856e8d15766
Jackie Ng <jumpinjackie@gmail.com>
2014-08-25 10:53:43 +1000

    Check in a binary distribution of swig v3.0.2. This is a subset of the swig distribution, sans source, examples, docs and other non-essential files to reduce checkin/checkout footprint.

--- Lang/DotNet/build.bat
+++ Lang/DotNet/build.bat
@@ -4,5 +4,5 @@ if "%1" == "x64" SET PLAT=x64
 if exist Bin rd /S /Q Bin
 del /F /Q Wrapper\*.cs
 rem swig -csharp -c++ -nodefaultctor -nodefaultdtor -namespace OSGeo.FDO -outdir "%CD%\Wrapper" -o "%CD%\FdoDotNet\fdo_wrap.cpp" -I"%CD%\..\..\Fdo\Inc" fdo.i
-swig -csharp -c++ -nodefaultctor -nodefaultdtor -namespace OSGeo.FDO -outdir "%CD%\Wrapper" -o "%CD%\FdoDotNet\fdo_wrap.cpp" -I"%CD%\..\..\Fdo\Inc" fdo.i 1> swig_build.log 2>&1
+..\..\Tools\swigwin-3.0.2\swig.exe -csharp -c++ -nodefaultctor -nodefaultdtor -namespace OSGeo.FDO -outdir "%CD%\Wrapper" -o "%CD%\FdoDotNet\fdo_wrap.cpp" -I"%CD%\..\..\Fdo\Inc" fdo.i 1> swig_build.log 2>&1
 msbuild FdoDotNet.sln /p:Configuration=Release;Platform=%PLAT%
--- Lang/Python/build.bat
+++ Lang/Python/build.bat
@@ -5,5 +5,5 @@ if "%1" == "x64" SET PLAT=x64
 if exist "Bin\%PLAT%\%CONFIG%" del /Q /F "Bin\%PLAT%\%CONFIG%\_PyFDO.*"
 if exist "Bin\%PLAT%\%CONFIG%" del /Q /F "Bin\%PLAT%\%CONFIG%\PyFDO.*"
 mkdir "Bin\%PLAT%\%CONFIG%"
-swig -python -c++ -nodefaultctor -nodefaultdtor -outdir "%CD%\Bin\%PLAT%\%CONFIG%" -o "%CD%\FdoPython\fdo_wrap.cpp" -I"%CD%\..\..\Fdo\Inc" fdo.i 1> swig_build.log 2>&1
+..\..\Tools\swigwin-3.0.2\swig.exe -python -c++ -nodefaultctor -nodefaultdtor -outdir "%CD%\Bin\%PLAT%\%CONFIG%" -o "%CD%\FdoPython\fdo_wrap.cpp" -I"%CD%\..\..\Fdo\Inc" fdo.i 1> swig_build.log 2>&1
 msbuild FdoPython.sln /p:Configuration=Release;Platform=%PLAT%
