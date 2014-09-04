6ea09ecab5cc1646d07e040c97903fc970ea9f02
Jackie Ng <jumpinjackie@gmail.com>
2014-09-04 18:28:31 +1000

    Add Linux build support for Python binding

---
+++ Lang/Python/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+mkdir -p Bin/x86/Release
+/usr/local/bin/swig -python -c++ -nodefaultctor -nodefaultdtor -outdir "Bin/x86/Release" -o "FdoPython/fdo_wrap.cpp" -I/usr/local/fdo-3.9.0/include fdo.i
+pushd FdoPython
+make
+make install
+popd
