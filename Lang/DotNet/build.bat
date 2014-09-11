10c6d09f2b6ed7e65ab75b7893a2fd8d5fadc928
Jackie Ng <jumpinjackie@gmail.com>
2014-09-11 22:47:24 +1000

    Specify nodefaultctor and nodefaultdtor from FdoIncludes.i, this ensures SWIG will generate a dtor for FdoDateTime (which doesn't have one), but nothing for everything else. Fixes #12

--- Lang/DotNet/build.bat
+++ Lang/DotNet/build.bat
@@ -6,7 +6,7 @@ if "%1" == "all" SET BUILD_ALL=1
 if exist Bin rd /S /Q Bin
 del /F /Q Wrapper\*.cs
 rem swig -csharp -c++ -nodefaultctor -nodefaultdtor -namespace OSGeo.FDO -outdir "%CD%\Wrapper" -o "%CD%\FdoDotNet\fdo_wrap.cpp" -I"%CD%\..\..\Fdo\Inc" fdo.i
-..\..\Tools\swigwin-3.0.2\swig.exe -csharp -c++ -nodefaultctor -nodefaultdtor -namespace OSGeo.FDO -outdir "%CD%\Wrapper" -o "%CD%\FdoDotNet\fdo_wrap.cpp" -I"%CD%\..\..\Fdo\Inc" fdo.i 1> swig_build.log 2>&1
+..\..\Tools\swigwin-3.0.2\swig.exe -csharp -c++ -namespace OSGeo.FDO -outdir "%CD%\Wrapper" -o "%CD%\FdoDotNet\fdo_wrap.cpp" -I"%CD%\..\..\Fdo\Inc" fdo.i 1> swig_build.log 2>&1
 if "%BUILD_ALL%" == "1" (
     msbuild FdoDotNet.sln /p:Configuration=Release;Platform=x86
     msbuild FdoDotNet.sln /p:Configuration=Release;Platform=x64
--- Lang/Python/build.sh
+++ Lang/Python/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p Bin/x86/Release
-/usr/local/bin/swig -python -c++ -nodefaultctor -nodefaultdtor -outdir "Bin/x86/Release" -o "FdoPython/fdo_wrap.cpp" -I/usr/local/fdo-3.9.0/include fdo.i
+/usr/local/bin/swig -python -c++ -outdir "Bin/x86/Release" -o "FdoPython/fdo_wrap.cpp" -I/usr/local/fdo-3.9.0/include fdo.i
 pushd FdoPython
 make
 make install
